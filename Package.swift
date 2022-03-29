34cf7fa973d240b48bbfb8cdbf746ba35032c6e8
Ivan O <ioparin@pm.me>
2022-03-30 00:15:11 +0400

    finalizing package manifest

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "GalenitTensorFlow",
+    name: "galenit-tensorflow",
     platforms: [
         .iOS(.v14)
     ],
@@ -27,17 +27,17 @@ let package = Package(
         ),
         .binaryTarget(
             name: "TensorFlowLiteC",
-            url: "https://github.com/Galen-IT/FlowVit_PublicSDKs_iOS/releases/download/v1.0.0/TensorFlowLiteC.xcframework.zip",
+            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.0/TensorFlowLiteC.xcframework.zip",
             checksum: "5f90d1c0b4788b715d9778011d9d513ac22a8bd3d74b7a1fa236ab33242288f7"
         ),
         .binaryTarget(
             name: "TensorFlowLiteCCoreML",
-            url: "https://github.com/Galen-IT/FlowVit_PublicSDKs_iOS/releases/download/v1.0.0/TensorFlowLiteCCoreML.xcframework.zip",
+            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.0/TensorFlowLiteCCoreML.xcframework.zip",
             checksum: "931d0a7b03d2f862685ddb52ceaa9f79877d749470193d9520bda209b9e78a0b"
         ),
         .binaryTarget(
             name: "TensorFlowLiteCMetal",
-            url: "https://github.com/Galen-IT/FlowVit_PublicSDKs_iOS/releases/download/v1.0.0/TensorFlowLiteCMetal.xcframework.zip",
+            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.0/TensorFlowLiteCMetal.xcframework.zip",
             checksum: "7c7be5b84e99bd7a834d998294c7c950c2e838c3bf7cbfaea99a9415c396b86f"
         )
     ]
