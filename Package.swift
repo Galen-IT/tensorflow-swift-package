c7ca2fdf8cf32572668b11b03716a6bb2f7d1932
Ivan O <ioparin@pm.me>
2022-04-26 22:03:00 +0400

    updating package with support for apple silicone simulator builds

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GalenitTensorFlow",
-            url: "https://github.com/galen-it/tensorflow-swift-package/releases/download/2.7.0/GalenitTensorFlow.xcframework.zip",
-            checksum: "110ab8194f8c4d8fd72592d200f4ada5d8c7a9ef75edfea09bcad6ec5d435af5"
+            url: "https://github.com/galen-it/tensorflow-swift-package/releases/download/2.7.10/GalenitTensorFlow.xcframework.zip",
+            checksum: "ba7f89245ac2719bebac0eb3901ea6006da7163a252992c0b1f39565945d1fe1"
         ),
     ]
 )
